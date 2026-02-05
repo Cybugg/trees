907a7edda81705991a10b9529d839ff78de54a8b
cybugg <timehinsamuel11@gmail.com>
2026-02-05 19:45:36 +0100

    added breadth search in js

--- b_tree/bTree.js
+++ b_tree/bTree.js
@@ -31,20 +31,54 @@ c.left = f;
 //      / \   / \
 //     d   e f   g
 
-const depthFirstValues = (root) => {
-    const stack = [root];
-    const results = [];
 
-    while(stack.length > 0){
-       const current = stack.pop();
-        results.push(current.val)
-        console.log(current.val)
-        if(current.right) stack.push(current.right);
-        if(current.left) stack.push(current.left);
+// using stack for the traverssal depth search
+// const depthFirstValues = (root) => {
+//     // check for edge cases
+//     if (!root) return [];
+//     const stack = [root];
+//     const results = [];
 
-    }
+//     while(stack.length > 0){
+//        const current = stack.pop();
+//         results.push(current.val)
+//         console.log(current.val)
+//         if(current.right) stack.push(current.right);
+//         if(current.left) stack.push(current.left);
+
+//     }
+// }
+
+
+// Using recursion method for the traversal depth search
+
+const depthFirstValues = (root)=>{
+    if(!root) return [];
+    const leftValues = depthFirstValues(root.left);
+    const rightValues = depthFirstValues(root.right);
+    return [root, ...leftValues, ...rightValues];
 }
 
+// using queue for the breadth traversal search
+const breadthFirstValue = (root)=>{
+    if(!root)return [];
+    const queue = [root];
+    const results = [];
+    while(queue.length > 0){
+        const current = queue.shift();
+        console.log(current.val);
+        results.push(current.val)
+        if(current.left)queue.push(current.left);
+        if(current.right)queue.push(current.right);
+    }
+    return results;
+}
+
+// Lets use recursion for the breadth first search
 
 
-depthFirstValues(a);
+console.log("## Depth first search: ##");
+// console.log(
+// depthFirstValues(a))
+console.log("## Breadth first search: ##");
+console.log(breadthFirstValue(a));
